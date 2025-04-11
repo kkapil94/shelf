8969c13026664939c1217785f4855c56c6bd9d24
Kapil <khatrikapil987@gmail.com>
2025-04-11 15:20:28 +0530

    feat: add logger + async handler for cleaner error tracking

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,29 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import morgan from "morgan";
+import logger from "./utils/logger";
 
 const app: Application = express();
+const morganFormat = ":method :url :status :response-time ms";
 
 app.use(express.json({ limit: "50kb" }));
 app.use(express.urlencoded({ limit: "50kb", extended: true }));
 app.use(cors({ credentials: true, origin: "*" }));
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 app.get("/api/v1", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
